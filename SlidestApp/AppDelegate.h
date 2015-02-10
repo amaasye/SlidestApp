211a3cf235db9c17606316deea9bb3024e4ad5e4
Syed Amaanullah <syedamaanullah@Syeds-MBP.attlocal.net>
2015-02-09 23:03:04 -0600

    Initial Commit

---
+++ SlidestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlidestApp
+//
+//  Created by Syed Amaanullah on 2/9/15.
+//  Copyright (c) 2015 Mateusz Pis & Syed Amaanullah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
