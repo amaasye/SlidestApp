a12334a2881a9fed201962d2c799c88260db18f4
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-02-10 09:49:49 -0600

    added Dropbox chooser integration

--- SlidestApp/CreateViewController.h
+++ SlidestApp/CreateViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface CreateViewController : UIViewController
+@property NSData *dataFromDropbox;
 
 @end
--- SlidestApp/CreateViewController.m
+++ SlidestApp/CreateViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "CreateViewController.h"
+#import <DBChooser/DBChooser.h>
+
 
 @interface CreateViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
@@ -22,10 +24,25 @@
 }
 
 - (IBAction)onUploadButtonTapped:(UIButton *)sender {
+    [[DBChooser defaultChooser] openChooserForLinkType:DBChooserLinkTypePreview
+                                    fromViewController:self completion:^(NSArray *results)
+     {
+         if ([results count]) {
+             DBChooserResult *chooser = results.firstObject;
+             [self downloadPDF:chooser];
+         } else {
+             // User canceled the action
+         }
+     }];
 
 }
 
-
+- (void)downloadPDF:(DBChooserResult *)chooser {
+    NSURLRequest *request = [NSURLRequest requestWithURL:chooser.link];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        self.dataFromDropbox = data;
+    }];
+}
 
 
 
