8949a62e1400795283a4c3daff02e12797a44448
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-02-13 10:19:16 -0600

    added data handler and refactored code

---
+++ SlidestApp/DataHandler.h
@@ -0,0 +1,28 @@
+//
+//  DataHandler.h
+//  SlidestApp
+//
+//  Created by Syed Amaanullah on 2/13/15.
+//  Copyright (c) 2015 Mateusz Pis & Syed Amaanullah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <DBChooser/DBChooser.h>
+#import <Parse/Parse.h>
+
+@protocol DataHandlerDelegate
+@optional
+- (void)downloadingShouldStart;
+- (void)downloadingShouldEnd;
+- (void)fileIsPDF:(BOOL)isPDF withName:(NSString *)name;
+
+@end
+@interface DataHandler : NSObject
+@property PFObject *slideshow;
+@property NSString *name;
+@property id<DataHandlerDelegate> delegate;
+@property NSData *dataFromDropbox;
+- (void)downloadPDF:(DBChooserResult *)chooser;
+- (void)pushDataToParse:(NSString *)passcode;
+
+@end
