9123e81b797bba827c0facc9c3143cebfad1c284
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-02-23 10:20:04 -0600

    revert changes on this branch

---
+++ SlidestApp/JoinViewController.h
@@ -0,0 +1,15 @@
+//
+//  JoinViewController.h
+//  SlidestApp
+//
+//  Created by Syed Amaanullah on 2/9/15.
+//  Copyright (c) 2015 Mateusz Pis & Syed Amaanullah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DataHandler.h"
+
+@interface JoinViewController : UIViewController
+@property DataHandler *datahandler;
+
+@end
